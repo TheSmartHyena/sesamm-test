e009385e3bfd1528b57a759367691a62a2c078ae
Philippe Skopal <58039366+TheSmartHyena@users.noreply.github.com>
2023-02-21 22:33:18 +0100

    Cleaning and enter key search trigger

--- src/components/CityComponent.tsx
+++ src/components/CityComponent.tsx
@@ -8,10 +8,16 @@ export const CityComponent = () => {
         dispatch({ type: ReducerActionList.SET_CITY, payload: e.target.value });
     }
 
+    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === 'Enter'){
+            dispatch({ type: ReducerActionList.TRIGGER_SEARCH });
+        }
+    }
+
     return (
         <>
             <p>Ville: </p>
-            <input type="text" value={state.city} onChange={handleCityChange} disabled={state.token?.length === 0 || state.loadingCities} />
+            <input type="text" value={state.city} onChange={handleCityChange} onKeyDown={handleKeyPress} disabled={state.token?.length === 0 || state.loadingCities} />
         </>
     )
 }
